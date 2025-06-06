76177fd4efb8c937f71716516c092e7dde0e2f4a
ScarlettRiquelme <sc.riquelmej@duocuc.cl>
2025-06-06 10:49:09 -0400

    versión 2
    cambio con una lista con 100 números aleatorio

---
+++ creacion_numeros aleatorios.py
@@ -0,0 +1,9 @@
+import random #libreria para llamar y crear numeros random
+
+numeros=[] #coleccion vacia
+
+for i in range(100): #ciclo para repetir 100 veces las interaciones
+    n = random.randint(1,100) #crea el numero entre 1 y 100
+    numeros.append(n) #guardamos en la lista el numero aleatorio
+
+print(numeros) #visualisamos lista
